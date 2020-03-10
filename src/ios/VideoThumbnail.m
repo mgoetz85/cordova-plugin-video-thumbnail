bfb3265d13ede2ed29035079b00216772177d8c2
mgoetz85 <marcus-goetz@gmx.de>
2020-03-10 09:13:27 +0100

    Update VideoThumbnail.m

--- src/ios/VideoThumbnail.m
+++ src/ios/VideoThumbnail.m
@@ -38,7 +38,7 @@ if(![saveFolder hasSuffix:@"/"]){
 }
 NSTimeInterval  now=   [NSDate date].timeIntervalSince1970;
 NSString* nowString=[NSString stringWithFormat:@"%.0f",now];
-NSString* randomVal=[NSString stringWithFormat:@"%.0f", (arc4random()%100000)+1];
+NSString* randomVal=[[NSProcessInfo processInfo] globallyUniqueString];
 NSString * savePath=[NSString stringWithFormat:@"%@thumbnail_%@_%@.jpg",saveFolder,nowString,randomVal];
 [self.commandDelegate runInBackground:^{
   CDVPluginResult* pluginResult=nil;
